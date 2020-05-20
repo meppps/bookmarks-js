aa11a48bfdff2b13afe13955a09b2a9a92fc6b25
Michael Epps <meppscomplex@gmail.com>
2020-05-19 22:59:19 -0700

    Some fixes

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ var width = '30';
 var height = '30';
 
 
+// function initBm(){
+var bookmark1 = {
+    site: 'site',
+    title: 'title',
+    url: 'url'
+}
+
+var bookmark2 = {
+    site: 'site2',
+    title:'title2',
+    url:'url2'
+}
+
+var firstBMS = [bookmark1,bookmark2];
+
+chrome.storage.sync.set({bookmarks:JSON.stringify(firstBMS)});
+chrome.storage.sync.get(['bookmarks'],(data)=>{console.log(data)});
+// console.log(bookmarks)
+// }
+// initBm();
+
+
 
 mainBtn.style.cssText = `background: ${background}; width: ${width}px; height: ${height}px; position: fixed;bottom:0;left: 0;color:black`;
 
@@ -27,6 +49,7 @@ document.body.appendChild(mainBtn);
 document.body.appendChild(fetchBtn);
 document.body.appendChild(addBtn);
 
+
 function addBookMark(){
     
     const site = window.location.host;
@@ -41,30 +64,66 @@ function addBookMark(){
     }; 
 
 
-    console.log(bookmark);
+    console.log(site,title,url);
 
-    function fetch(){
-        // 
-        var test = chrome.storage.sync.get(['test'],(data)=>{
-            // console.log(JSON.parse(data))
-            console.log(JSON.parse(data.test))
-        })
-            return item;
+
+    
+    // Add Bookmark
+    chrome.storage.sync.get(['bookmarks'],(data)=>{
+
+        
+        if(data.bookmarks){
+            var bookmarks = JSON.parse(data.bookmarks);
+            bookmarks.push(bookmark);
+            chrome.storage.sync.set({test:JSON.stringify(bookmarks)});
+        }else{
+            var bookmarks = [];
+            bookmarks.push(bookmark);
+            chrome.storage.sync.set({test:JSON.stringify(bookmarks)});
         }
-    };
+
+    });
 
 
-    if(chrome.storage.sync.get(['bookmarks']) == null){
-        var bookmarks = []
-        bookmarks.push(bookmark);
-        chrome.storage.sync.set({bookmarks:JSON.stringify(bookmarks)});
-    } else {
-        var bookmarks = JSON.parse(chrome.storage.sync.get(['bookmarks']));
-        bookmarks.push(bookmark);
-        chrome.storage.sync.set({bookmarks:JSON.stringify(bookmarks)});
-    }
 };
 
+
+
+// Show bookmarks 
+
+function showBookmarks(bookmarks){
+    console.log('%c show bookmarks', 'color: yellow');
+    chrome.storage.sync.get(['bookmarks'],(data)=>{
+        console.log(data)
+
+        var bookmarks = JSON.parse(data.bookmarks);
+
+        console.log(bookmarks);
+
+        var table = document.createElement('table');
+        table.classList.add('bookmarkTable');
+        table.innerHTML = `
+        <tr><th>Site</th><th>Page</th><th>URL</th></tr>
+        `
+        document.body.appendChild(table);
+
+        bookmarks.forEach((mark)=>{
+            var row = document.createElement('tr');
+            row.innerHTML = `
+            <td>${mark.site}</td><td>${mark.title}</td><td>${mark.url}</td><td class="remove">X</td>
+            `;
+            table.appendChild(row)
+
+    });
+
+})};
+
+// document.addEventListener('load',()=>{
+
+    fetchBtn.addEventListener('click',()=>{
+        console.log('hfdshaf;lkjs')
+        showBookmarks();
+        });
 
 
 
@@ -75,70 +134,61 @@ function initRm(){
         rm.addEventListener('click',()=>{
             var url = rm.parentElement.childNodes[3].innerText;
             remove(url);
-
-            document.querySelector('table.bookmarkTable').remove();
+            if(document.querySelector('table.bookmarkTable')){
+                document.querySelector('table.bookmarkTable').remove();
+            }
             showBookmarks();
 
-            console.log(fetch())
+            // console.log(fetch())
         })
     });
 };
+    // 
 
-function showBookmarks(){
-    var bookmarks = fetch();
-    console.log(bookmarks);
 
-    var table = document.createElement('table');
-    table.classList.add('bookmarkTable');
-    table.innerHTML = `
-    <tr><th>Site</th><th>Page</th><th>URL</th></tr>
-    `
-    document.body.appendChild(table);
 
-    bookmarks.forEach((mark)=>{
-        var row = document.createElement('tr');
-        row.innerHTML = `
-        <td>${mark.site}</td><td>${mark.title}</td><td>${mark.url}</td><td class="remove">X</td>
-        `;
-        table.appendChild(row)
-
-    });
-    initRm();
-}
 
 
 
 function remove(url){
 
-    var bookmarks = fetch();
+    // test > bookmarks 
+    chrome.storage.sync.get(['bookmarks'],(data)=>{ 
 
+    var bookmarks = JSON.parse(data.bookmarks)
+    
     for(i=0;i<bookmarks.length;i++){
 
+
+        
         var bm = bookmarks[i];
         // console.log(bm.url)
         if(url === bm.url){
             bookmarks.splice(i,1);
-            chrome.storage.sync.get('bookmarks',JSON.stringify(bookmarks));
+            chrome.storage.sync.set({bookmarks:JSON.stringify(bookmarks)});
+            // chrome.storage.sync.get('bookmarks',JSON.stringify(bookmarks));
             return;
         }
 
         
-    }
-    
+    } 
+});
 }
 
+function clearTable(){
+    if(document.querySelector('.bookmarkTable')){
+    document.querySelector('.bookmarkTable').remove();
+    }
+};
 
-
-fetchBtn.addEventListener('click',()=>{
-    showBookmarks();
-
- 
-    });
 
 // 
 addBtn.addEventListener('click',()=>{
     addBookMark();
-    document.querySelector('table.bookmarkTable').remove();
+    clearTable();
     showBookmarks();
-
+    
 });
+
+
+// })};
--- temp/index.js
+++ temp/index.js
@@ -16,19 +16,36 @@ var bookmarks = [bookmark1,bookmark2];
 
 chrome.storage.sync.set({test:JSON.stringify(bookmarks)});
 // chrome.storage.sync.set({test:'this is some words'})
-
-// var test = chrome.storage.sync.get(['test'],(data)=>{
+// chrome.storage.sync.get(['test'],(data)=>{
 //     // console.log(JSON.parse(data))
-//     console.log(JSON.parse(data.test))
+//      output = JSON.parse(data.test);
+//     // console.log()
+    
+//     // console.log(output)
+
+//     console.log(output)
 // })
 
-function testing(){
- var test = chrome.storage.sync.get(['test'],(data)=>{
-    // console.log(JSON.parse(data))
-    console.log(JSON.parse(data.test));
-    return JSON.parse(data.test)
-})}
 
-testing();
-// var test = JSON.parse(chrome.storage.sync.get(['test']));
-// console.log(test);
+
+    var output;
+
+   var ret = chrome.storage.sync.get(['test'],(data)=>{
+        // console.log(JSON.parse(data))
+         output = JSON.parse(data.test);
+        // console.log()
+        
+        // console.log(output)
+
+        // console.log(output)
+        return output;
+    });
+
+    console.log(ret)
+    
+
+// getBMS();
+// var ret = getBMS();
+// console.log(ret);
+
+
